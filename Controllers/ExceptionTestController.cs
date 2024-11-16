5fbf444d5f0d1c464bc8a2ce901ab196b74789b5
ofurkanmeral <fom1329@gmail.com>
2024-11-16 11:36:15 +0300

    Add project files.

---
+++ Controllers/ExceptionTestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddleeareHandling_ExceptionAndLogging.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExceptionTestController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            throw new System.Exception("GlobalException Test");
+            return "Debug Sonucu Gelen HttpRequest Middleware Kontrolu";
+        }  
+    }
+}
---
+++ Exception/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddleeareHandling_ExceptionAndLogging.Exception
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger<ExceptionHandlerMiddleware> logger;
+
+        public ExceptionHandlerMiddleware(RequestDelegate _next, ILogger<ExceptionHandlerMiddleware> _logger)
+        {
+            next = _next;
+            logger = _logger;
+        }
+
+       
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await next.Invoke(httpContext);
+            }
+            catch (System.Exception ex)
+            {
+                logger.LogError(ex.Message.ToString());
+                throw;
+            }
+        }
+    }
+}
